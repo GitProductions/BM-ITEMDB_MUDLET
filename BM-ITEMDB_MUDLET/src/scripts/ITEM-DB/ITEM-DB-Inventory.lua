9d5efc7e34d67917a06ab9f86a42b96244cb1866
Gitago <76603653+gitagogaming@users.noreply.github.com>
2026-02-01 23:53:57 -0500

    Refactor inventory window and update version info
    Refactored the inventory window logic to improve label/button creation and added a refresh function for dynamic updates.
    
    . Added comments and addressed UI issues in TEST_WINDOW.lua.

--- BM-ITEMDB_MUDLET/src/scripts/ITEM-DB/ITEM-DB-Inventory.lua
+++ BM-ITEMDB_MUDLET/src/scripts/ITEM-DB/ITEM-DB-Inventory.lua
@@ -301,7 +301,7 @@ function itemdb.inventory.refresh()
     text = [[<div style="padding: 4px 0;">]] .. text .. [[</div>]]
 
 
-    -- itemdb.inventory.window.box.label:echo(text)
+    -- itemdb.window.box.label:echo(text)
     itemdb.inventory.contentLabel:echo(text)
 end
 
--- BM-ITEMDB_MUDLET/src/scripts/ITEM-DB/TEST_WINDOW.lua
+++ BM-ITEMDB_MUDLET/src/scripts/ITEM-DB/TEST_WINDOW.lua
@@ -1,10 +1,16 @@
-itemdb.inventory.window = Adjustable.Container:new({
+------- Issues..
+-- 1.  Sometimes the ui needs to be moved/adjusted for it to initially populate the data its been given.
+-- 2. After reloading the module, it causes the old labels/buttons to still remain and cover parts of the screen until restarting the users profile
+-- 3. Contents are not scrollable and overflow if too long for window height.  - adding overflow to container doesnt help
+
+
+itemdb.inventory.window = itemdb.inventory.window or  Adjustable.Container:new({
     name = "myWindow",
     x = "75%",
     y = "2%",
     width = "22%",
     height = "35%",
-    titleText = "Inventory v2",
+    titleText = "Inventory - v2",
     titleTxtColor = "#c8d6e5",
     adjLabelstyle = [[
         background-color: rgb(20, 22, 28);
@@ -14,32 +20,38 @@ itemdb.inventory.window = Adjustable.Container:new({
 
 })
 
-itemdb.inventory.window.box = Geyser.Label:new({
-    name = "itemdb.inventory.window.box",
-    x = 0,
-    y = 0,
-    width = "100%",
-    height = "100%"
-}, itemdb.inventory.window)
 
--- local rows = {{
---     name = "Health",
---     cond = "OK"
--- }, {
---     name = "Mana",
---     cond = "Low"
--- }, {
---     name = "Stamina",
---     cond = "Full"
--- }, {
---     name = "Energy",
---     cond = "Good"
--- }}
+-- adding padding to this container does not effect the content which resides inside of it?
+-- so how do i add room between the outside of the box and the content inside of it? 
+-- default color:     background-color: rgb(20, 22, 28);
+-- itemdb.inventory.window.box = itemdb.inventory.window.box or Geyser.Label:new({
+--     name = "itemdb.inventory.window.box",
+--     x = 0,
+--     y = 0,
+--     width = "100%",
+--     height = "100%"
+-- }, itemdb.inventory.window)
 
--- 
+    -- default color:     background-color: rgb(20, 22, 28);
+-- itemdb.inventory.window.box:setStyleSheet([[
+--     margin: 20px;
+--     background-color: rgb(14, 63, 168);
+-- ]])
+
+
+---- attempted to use scrollable box to prevent content from overflowing when window isnt sized properly.. didnt work :( 
+-- itemdb.inventory.window.box.scrollable = itemdb.inventory.window.box.scrollable or Geyser.ScrollBox:new({
+--     name = "itemdb.inventory.window.box.scrollable",
+--     x = 0,
+--     y = 0,
+--     width = "100%",
+--     height = "100%"
+-- }, itemdb.inventory.window.box)
+
+
 
 local rows = {{
-    name = "dagger",
+    name = "daggers",
     condition = "excellent",
     quantity = 15,
     desc = nil
@@ -50,71 +62,53 @@ local rows = {{
     desc = nil
 }}
 
-for i, item in ipairs(rows) do
-    local yOff = (i - 1) * 30 -- offset in px
+function itemdb.inventory.window.refresh()
+    -- send("inv")
 
-    cecho("The offset is " .. yOff .. "\n")
+    for i, item in ipairs(itemdb.inventory.data) do
+        local yOff = (i - 1) * 30
 
-    -- left label
-    itemdb.inventory.window.box.label = Geyser.Label:new({
-        name = "itemdb.inventory.window.label." .. i,
-        x = "0px",
-        y = yOff,
-        width = "60%",
-        height = "25px",
-        message = item.name .. " ( " .. item.condition .. " )"
-    }, itemdb.inventory.window.box)
+        -- build label text safely
+        local label_text = item.name .. " (" .. item.condition .. ")"
 
-    itemdb.inventory.window.box.label:setStyleSheet([[
-        background-color: #2b2b2b;
-        padding-left: 10px;
-        font-weight: 600
-    ]])
+        if item.quantity and item.quantity >= 2 then
+            label_text = label_text .. " [" .. item.quantity .. "]"
+        end
 
-    -- right button
-    itemdb.inventory.window.box.label = Geyser.Button:new({
-        name = "itemdb.inventory.window.button." .. i,
-        msg = "<center>Select Item</center>",
-        clickCommand = "look " .. item.name,
-        x = "60%",
-        y = yOff,
-        width = "40%",
-        height = "25px",
-        style = [[ margin: 1px; background-color: black; border: 1px solid white; ]]
-    }, itemdb.inventory.window.box)
+        -- left label
+        Geyser.Label:new({
+            name = "itemdb.inventory.window.label." .. i,
+            x = "0px",
+            y = yOff,
+            width = "80%",
+            height = "25px",
+            message = label_text
+        }, itemdb.inventory.window):setStyleSheet([[
+            background-color: #2b2b2b;
+            padding-left: 10px;
+            font-weight: 600
+        ]])
+
+        -- right button
+        Geyser.Button:new({
+            name = "itemdb.inventory.window.button." .. i,
+            msg = "<center>Select</center>",
+            clickCommand = "look " .. item.name,
+            clickFunction = function()
+                itemdb.submitCapturedItem(item.name)
+            end,
+            x = "80%",
+            y = yOff,
+            width = "20%",
+            height = "25px",
+            style = [[
+                margin: 1px;
+                background-color: black;
+                border: 1px solid white;
+            ]]
+        }, itemdb.inventory.window)
+    end
 end
 
--- for i, info in ipairs(rows) do
---     local yOff = (i - 1) * 30 -- offset in px
 
---     cecho("The offset is " .. yOff .. "\n")
-
---     -- left label
---     itemdb.inventory.window.box.label = Geyser.Label:new({
---         name = "itemdb.inventory.window.label." .. i,
---         x = "0px",
---         y = yOff,
---         width = "60%",
---         height = "25px",
---         message = info.name .. " / " .. info.cond
---     }, itemdb.inventory.window.box)
-
---     itemdb.inventory.window.box.label:setStyleSheet([[
---         background-color: #2b2b2b;
---         padding-left: 10px;
---         font-weight: 600
---     ]])
-
---     -- right button
---     itemdb.inventory.window.box.label = Geyser.Button:new({
---         name = "itemdb.inventory.window.button." .. i,
---         msg = "<center>Select Item</center>",
---         clickCommand = "look " .. info.name,
---         x = "60%",
---         y = yOff,
---         width = "40%",
---         height = "25px",
---         style = [[ margin: 1px; background-color: black; border: 1px solid white; ]]
---     }, itemdb.inventory.window.box)
--- end
 
